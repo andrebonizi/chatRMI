888867f4d016511224e6eda0bc3d59573a5f2e9a
André Bonizi <andrebonizi@gmail.com>
2020-05-12 22:29:53 -0300

    Update ClientRMIGUI.java

--- src/client/ClientRMIGUI.java
+++ src/client/ClientRMIGUI.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package client;
 
 import java.awt.BorderLayout;
@@ -45,7 +41,7 @@ import javax.swing.border.EmptyBorder;
 
 /**
  *
- * @author Azkar
+ * @author Azkar and André Bonizi
  */
 public class ClientRMIGUI extends JFrame implements ActionListener{
 	
